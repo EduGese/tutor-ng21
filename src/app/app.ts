92d0721a91d9a157b5bfc65db5fa0d0aefffbed6
EduGese <122921699+EduGese@users.noreply.github.com>
2025-11-27 11:03:04 +0100

    feat: Implement core recipe display and serving adjustment
     This commit introduces the foundational elements for the Smart Recipe Box application:
     - Initializes basic application structure with dynamic title.
     - Implements recipe data display using signals and mock data.
     - Adds functionality to switch between recipes using button clicks.
     - Introduces a 'servings' signal with increment/decrement controls,
       preventing servings from falling below zero.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tutor-ng21');
+  protected readonly title = signal<string>('My recipe Box');
+  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+  protected readonly servings = signal<number>(0);
+
+  protected setRecipe1() {
+    this.recipe.set(MOCK_RECIPES[0]);
+  }
+   protected setRecipe2() {
+    this.recipe.set(MOCK_RECIPES[1]);
+  }
+
+  protected incrementServigs(){
+    this.servings.update(current=> current +1 )
+  }
+    protected decrementServigs(){
+    this.servings.update(current=>{
+      if(current=== 0) return current ;
+      return current -1;
+    } )
+  }
 }
+
+
